5cf2298b6f4c4596178171a01021399a42ea0c5b
Yana Pavlova <yn-pvlv@yandex.ru>
2025-03-03 22:50:17 +0300

    feat: implement getCountWeekendsInMonth function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -168,8 +168,16 @@ function formatDate(date) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  let count = 0;
+  const daysInMonth = new Date(year, month, 0).getDate();
+
+  for (let day = 1; day <= daysInMonth; day += 1) {
+    const weekday = new Date(year, month - 1, day).getDay();
+    if (weekday === 0 || weekday === 6) count += 1;
+  }
+
+  return count;
 }
 
 /**
