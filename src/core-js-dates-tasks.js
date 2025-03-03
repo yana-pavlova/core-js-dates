1e3eba751f05b3d0bf43ca3a8120220afa9020c6
Yana Pavlova <yn-pvlv@yandex.ru>
2025-03-03 21:14:26 +0300

    feat: implement getTime function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -32,8 +32,12 @@ function dateToTimestamp(date) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const hours = date.getHours().toString().padStart(2, 0);
+  const minutes = date.getMinutes().toString().padStart(2, 0);
+  const seconds = date.getSeconds().toString().padStart(2, 0);
+
+  return `${hours}:${minutes}:${seconds}`;
 }
 
 /**
